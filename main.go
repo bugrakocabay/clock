0722c4ba6eefacd2981bcf8577ea62843321db8c
bugrakocabay <kocabaybugra@gmail.com>
2022-12-07 22:20:13 +0300

    feature

--- main.go
+++ main.go
@@ -9,60 +9,51 @@ import (
 )
 
 func main() {
+	usage := "[int] [-v]?"
 	rand.Seed(time.Now().UnixMicro())
-	args := os.Args
+	args := os.Args[1:]
 
-	if len(args) != 2 {
-		fmt.Println("Give input")
+	if len(args) < 1 {
+		fmt.Println(usage)
+		return
 	}
 
-	guess, err := strconv.Atoi(args[1])
+	guess, err := strconv.Atoi(args[0])
 	if err != nil {
-		fmt.Println("Give an integer")
+		fmt.Println(usage)
+		return
 	}
 	if guess < 0 {
-		fmt.Println("Give an positive integer")
+		fmt.Println(usage)
+		return
 	}
-	randM1 := "Good job!"
-	randM2 := "Impressive!"
-	randM3 := "Pure luck!"
-	randM4 := "Good shit booii!"
+	/*
+			randM1 := "Good job!"
+			randM2 := "Impressive!"
+			randM3 := "Pure luck!"
+			randM4 := "Good shit booii!"
 
-	randL1 := "get lost"
-	randL2 := "ghahaha noob"
-	randL3 := "go play with toys"
-	randL4 := "u gonna cry??"
-
-	for turn := 0; turn < 3; turn++ {
-		n := rand.Intn(guess + 1)
+		randL1 := "get lost"
+		randL2 := "ghahaha noob"
+		randL3 := "go play with toys"
+		randL4 := "u gonna cry??"
+	*/
+	var line string
+	for turn := 0; turn < 8; turn++ {
+		n := rand.Intn(11)
 		if turn == 0 && n == guess {
 			fmt.Println("WOOWW YOU WON ON FIRST TURN!!!!")
 			return
 		}
 
+		if args[1] == "-v" {
+			line += fmt.Sprintf("%d ", n)
+		}
+
 		if n == guess {
-			switch rand.Intn(4) {
-			case 0:
-				fmt.Println(randM1)
-			case 2:
-				fmt.Println(randM2)
-			case 3:
-				fmt.Println(randM3)
-			case 1:
-				fmt.Println(randM4)
-			}
+			fmt.Printf("%s YOU WON", line)
 			return
 		}
 	}
-
-	switch rand.Intn(4) {
-	case 0:
-		fmt.Println(randL1)
-	case 1:
-		fmt.Println(randL2)
-	case 2:
-		fmt.Println(randL3)
-	case 3:
-		fmt.Println(randL4)
-	}
+	fmt.Printf("%s YOU lostt", line)
 }
