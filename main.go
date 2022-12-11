1d6784c04fed728e295772f6d66b7bc27d04e143
bugrakocabay <kocabaybugra@gmail.com>
2022-12-11 19:49:24 +0300

    only evens

--- main.go
+++ main.go
@@ -2,58 +2,34 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"os"
 	"strconv"
-	"time"
 )
 
 func main() {
-	usage := "[int] [-v]?"
-	rand.Seed(time.Now().UnixMicro())
-	args := os.Args[1:]
-
-	if len(args) < 1 {
-		fmt.Println(usage)
+	if len(os.Args) < 3 {
+		fmt.Println("gimme two numbers")
 		return
 	}
 
-	guess, err := strconv.Atoi(args[0])
-	if err != nil {
-		fmt.Println(usage)
+	min, err1 := strconv.Atoi(os.Args[1])
+	max, err2 := strconv.Atoi(os.Args[2])
+	if err1 != nil || err2 != nil || min >= max {
+		fmt.Println("wrong numbers")
 		return
 	}
-	if guess < 0 {
-		fmt.Println(usage)
-		return
-	}
-	/*
-			randM1 := "Good job!"
-			randM2 := "Impressive!"
-			randM3 := "Pure luck!"
-			randM4 := "Good shit booii!"
 
-		randL1 := "get lost"
-		randL2 := "ghahaha noob"
-		randL3 := "go play with toys"
-		randL4 := "u gonna cry??"
-	*/
-	var line string
-	for turn := 0; turn < 8; turn++ {
-		n := rand.Intn(11)
-		if turn == 0 && n == guess {
-			fmt.Println("WOOWW YOU WON ON FIRST TURN!!!!")
-			return
+	var sum int
+	for i := min; i <= max; i++ {
+		if i%2 != 0 {
+			continue
 		}
+		sum += i
 
-		if args[1] == "-v" {
-			line += fmt.Sprintf("%d ", n)
-		}
-
-		if n == guess {
-			fmt.Printf("%s YOU WON", line)
-			return
+		fmt.Print(i)
+		if i < max-1 {
+			fmt.Print(" + ")
 		}
 	}
-	fmt.Printf("%s YOU lostt", line)
+	fmt.Printf(" = %d\n", sum)
 }
