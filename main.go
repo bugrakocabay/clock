ef997e129052ef0450f19ff76cf4aa776d998fda
bugrakocabay <kocabaybugra@gmail.com>
2022-12-19 23:11:58 +0300

    clock

--- main.go
+++ main.go
@@ -2,31 +2,120 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"os"
-	"strconv"
+	"github.com/inancgumus/screen"
+	"time"
 )
 
 func main() {
-	args := os.Args[1:]
-
-	for _, arg := range args {
-		num, err := strconv.Atoi(arg)
-		if err != nil {
-			continue
-		}
-
-		if IsPrime(num) {
-			fmt.Printf("%d ", num)
-		}
+	zero := [5]string{
+		"▉▉▉",
+		"▉ ▉",
+		"▉ ▉",
+		"▉ ▉",
+		"▉▉▉",
 	}
-}
-
-func IsPrime(value int) bool {
-	for i := 2; i <= int(math.Floor(float64(value)/2)); i++ {
-		if value%i == 0 {
-			return false
-		}
+	one := [5]string{
+		"▉▉ ",
+		" ▉ ",
+		" ▉ ",
+		" ▉ ",
+		"▉▉▉",
 	}
-	return value > 1
+	two := [5]string{
+		"▉▉▉",
+		"  ▉",
+		"▉▉▉",
+		"▉  ",
+		"▉▉▉",
+	}
+	three := [5]string{
+		"▉▉▉",
+		"  ▉",
+		"▉▉▉",
+		"  ▉",
+		"▉▉▉",
+	}
+	four := [5]string{
+		"▉ ▉",
+		"▉ ▉",
+		"▉▉▉",
+		"  ▉",
+		"  ▉",
+	}
+	five := [5]string{
+		"▉▉▉",
+		"▉  ",
+		"▉▉▉",
+		"  ▉",
+		"▉▉▉",
+	}
+	six := [5]string{
+		"▉▉▉",
+		"▉  ",
+		"▉▉▉",
+		"▉ ▉",
+		"▉▉▉",
+	}
+	seven := [5]string{
+		"▉▉▉",
+		"  ▉",
+		"  ▉",
+		"  ▉",
+		"  ▉",
+	}
+	eight := [5]string{
+		"▉▉▉",
+		"▉ ▉",
+		"▉▉▉",
+		"▉ ▉",
+		"▉▉▉",
+	}
+	nine := [5]string{
+		"▉▉▉",
+		"▉ ▉",
+		"▉▉▉",
+		"  ▉",
+		"▉▉▉",
+	}
+	type placeholder [5]string
+	colon := placeholder{
+		"   ",
+		" ░ ",
+		"   ",
+		" ░ ",
+		"   ",
+	}
+	digits := [...]placeholder{
+		zero, one, two, three, four, five, six, seven, eight, nine,
+	}
+
+	screen.Clear()
+
+	for {
+		screen.MoveTopLeft()
+
+		now := time.Now()
+		hour, min, sec := now.Hour(), now.Minute(), now.Second()
+		clock := [...]placeholder{
+			digits[hour/10], digits[hour%10],
+			colon,
+			digits[min/10], digits[min%10],
+			colon,
+			digits[sec/10], digits[sec%10],
+		}
+
+		for line := range clock[0] {
+			for index, digit := range clock {
+				next := clock[index][line]
+				if digit == colon && sec%2 == 0 {
+					next = "   "
+				}
+
+				fmt.Print(next, "  ")
+			}
+			fmt.Println()
+		}
+		fmt.Println()
+		time.Sleep(time.Second)
+	}
 }
