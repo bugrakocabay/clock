66d6d527938f7400eead4f0c1fba7841ce189c53
bugrakocabay <kocabaybugra@gmail.com>
2023-01-09 22:53:16 +0300

    game-storev2

--- game-store/main.go
+++ game-store/main.go
@@ -4,8 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
@@ -24,48 +22,8 @@ func main() {
 
 `)
 
-		if !in.Scan() {
+		if !in.Scan() || !runCmd(in.Text(), games, byID) {
 			break
-		}
-
-		fmt.Println()
-
-		cmd := strings.Fields(in.Text())
-		if len(cmd) == 0 {
-			continue
-		}
-
-		switch cmd[0] {
-		case "quit":
-			fmt.Println("bye!")
-			return
-
-		case "list":
-			for _, g := range games {
-				// printGame()
-				fmt.Printf("#%d: %-15q %-20s $%d\n",
-					g.id, g.name, "("+g.genre+")", g.price)
-			}
-
-		case "id":
-			if len(cmd) != 2 {
-				fmt.Println("wrong id")
-				continue
-			}
-
-			id, err := strconv.Atoi(cmd[1])
-			if err != nil {
-				fmt.Println("wrong id")
-				continue
-			}
-
-			g, ok := byID[id]
-			if !ok {
-				fmt.Println("sorry. I don't have the game")
-				continue
-			}
-
-			printGame(g)
 		}
 	}
 }
